6aeb186dc9182280932d11fd67d39393de578c11
Ilhan Negis <ilhan.negis@gmail.com>
2026-03-05 18:30:23 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+#TalkDom
+
+started.
---
+++ index.js
@@ -0,0 +1,34 @@
+(function() {
+
+  let target = null;
+  function reqListener() {
+    if (target) {
+      target.innerHTML = this.responseText;
+    }
+  }
+
+  const body = document.querySelector("body");
+
+  body.addEventListener("click", (e) => {
+    const el = e.target.closest("[eringen]");
+    if (el) {
+      const attr = el.getAttribute("eringen").split(' ');
+      target = document.querySelector(el.getAttribute("eringen-target"));
+      methods[attr[0]]?.(...attr.slice(1)) ?? console.error(`unknown message: ${attr[0]}`);
+    }
+  });
+
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+
+  const methods = {
+    get: (url) => { req.open("GET", url); req.send(); return true },
+  }
+
+}());
+
+function changeInner(m, u) {
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  req.open(m, u); req.send();
+}
